6aa8ba0c89161fbe5268cf3eb5dd9c6a187b7153
Chris Low <chrislowzhengxi@uchicago.edu>
2025-11-06 18:43:00 -0600

    Added user, user bio

--- cloudysky/cloudysky/urls.py
+++ cloudysky/cloudysky/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -24,4 +24,6 @@ urlpatterns = [
     path("dummypage", views.dummypage, name="index"),
     path("app/time", views.time_now),
     path("app/sum", views.sum_view),
+    path('admin/', admin.site.urls),
+    path('', include('app.urls')),
 ]
