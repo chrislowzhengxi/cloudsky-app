3f742456e38589fc6d8b32357f37019ac7e00925
Chris Low <chrislowzhengxi@uchicago.edu>
2025-11-06 21:43:27 -0600

    Solving Test 6

--- cloudysky/cloudysky/settings.py
+++ cloudysky/cloudysky/settings.py
@@ -122,5 +122,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Redirect to index page after successful login
-LOGIN_REDIRECT_URL = '/index.html'
+# Redirect users to the homepage after login/logout
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
