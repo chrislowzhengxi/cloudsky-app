bceb9e148824a6a37e38352cd65bfe02c2808f66
Chris Low <chrislowzhengxi@uchicago.edu>
2025-11-06 21:57:00 -0600

    Added middleware for migration

--- cloudysky/cloudysky/settings.py
+++ cloudysky/cloudysky/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'cloudysky.middleware.EnsureMigrationsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
