bd7405e517add6feab0539072df64ce3b2bdc586
Chris Low <chrislowzhengxi@uchicago.edu>
2025-12-05 17:10:02 -0600

    Added feed url

--- cloudysky/app/urls.py
+++ cloudysky/app/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('app/hidePost/', views.hide_post, name='hide_post'),
     path('app/hideComment/', views.hide_comment, name='hide_comment'),
     path('app/dumpFeed/', views.dump_feed, name='dump_feed'),
+    path('app/feed/', views.dump_feed, name='feed'),  # Alias for dumpFeed
     # HW5: HTML form views
     path('app/new_post/', views.new_post, name='new_post'),
     path('app/new_comment/', views.new_comment, name='new_comment'),
